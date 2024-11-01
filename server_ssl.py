eac5bed5bc782f6fe05e8f0a695fa8407bdb3764
Swam244 <123237756+Swam244@users.noreply.github.com>
2024-11-01 20:04:18 +0530

    Containerized application with SSL

--- server_ssl.py
+++ server_ssl.py
@@ -8,7 +8,7 @@ ssl_certfile = settings.SSL_CERTFILE
 if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=8000,
         ssl_keyfile=ssl_keyfile,
         ssl_certfile=ssl_certfile,
